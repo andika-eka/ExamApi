61064020120d2cccf0bfeef6f3bbe58801391270
andika-eka <andikaeka1201@gmail.com>
2025-12-26 23:35:18 +0800

    feat(openapi): add OpenAPI configuration
    - Add basic API info
    - Define JWT bearer auth scheme
    - Enable Swagger UI for API docs

---
+++ src/main/java/com/technicaltale/examapi/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.technicaltale.examapi;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Exam API",
+        version = "1.0",
+        description = "Documentation for Exam Management System"
+    ),
+    security = @SecurityRequirement(name = "bearerAuth") 
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT",
+    description = "Paste the JWT token from Keycloak here"
+)
+public class OpenApiConfig {
+    
+}
