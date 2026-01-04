2b238117fbc25b52d35cd1054ffc3d03c21c0f76
andika-eka <andikaeka1201@gmail.com>
2026-01-04 13:29:33 +0800

    refactor(entity): improve question entity design
    - Add Lombok @Getter/@Setter to entities
    - Change Question.attachmentUri type to URI
    - Change MultipleChoiceQuestion.correctOption to int

--- src/main/java/com/technicaltale/examapi/entity/Question.java
+++ src/main/java/com/technicaltale/examapi/entity/Question.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.persistence.*;
 import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import java.net.URI;
 
 @Entity
 @Table(name = "questions")
@@ -22,36 +25,15 @@ public abstract class Question {
     private UUID id;
 
     @Column(columnDefinition = "TEXT")
-    private String content;
-
-    private String attachmentUri; // The image/file link
+    @Setter @Getter private String content;
+    
+    @Column(columnDefinition = "varchar(255)")
+    @Setter @Getter private URI attachmentUri; // The image/file link
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "exam_id")
     @JsonIgnore
-    private Exam exam;
+    @Setter @Getter private Exam exam;
 
-    public void setExam(Exam exam) {
-        this.exam = exam;
-    }
 
-    public Exam getExam() {
-        return exam;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAttachmentUri() {
-        return attachmentUri;
-    }
-
-    public void setAttachmentUri(String attachmentUri) {
-        this.attachmentUri = attachmentUri;
-    }
 }
